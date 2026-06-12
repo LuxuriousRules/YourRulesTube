82b81cc8ddc51dfa4682a4cbcc84fac337c2fd89
Vladimir <sklyrenkovssv@gmail.com>
2026-06-12 23:12:35 +0300

    loadingAnimation

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import BurgerMenu from "../components/ui/BurgerMenu";
 import SearchBar from "../components/ui/SearchBar";
 import style from "./layout.module.scss"
 import  "./global.css";
-import Test from "../components/main/Test";
+
 
 
 const raleway = Raleway({
